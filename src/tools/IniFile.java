0b245352f67a7f05931206a777faad58ad99644c
Maël <mael.nogues@gmail.com>
2016-07-20 20:25:03 +0200

    Re-commit after workspace crash

--- src/tools/IniFile.java
+++ src/tools/IniFile.java
@@ -1,6 +1,10 @@
 /*
- * Some file written fast to kill some time... Do whatever you want with it ;)
- * But don't forget to mention my name somewhere at least !
+ * The GNU General Public License does not permit incorporating your program
+ * into proprietary programs. If your program is a subroutine library, you may
+ * consider it more useful to permit linking proprietary applications with the
+ * library. If this is what you want to do, use the GNU Lesser General Public
+ * License instead of this License. But first, please read
+ * <http://www.gnu.org/philosophy/why-not-lgpl.html>.
  * @author Maël Nogues mael.nogues@outlook.com
  */
 package tools;
